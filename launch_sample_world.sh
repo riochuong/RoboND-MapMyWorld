4f9154a9c3fa8d45a4e60b18a630eb11161c5928
chuongdao <cd.kernel.dev.105@gmail.com>
2018-03-24 01:59:57 -0700

    fix project typos

--- launch_sample_world.sh
+++ launch_sample_world.sh
@@ -1,1 +1,7 @@
-roslaunch slam_project world.launch world_file:=/home/chuondao/catkin_ws/src/RoboND-Term2-MapMyWorld/worlds/kitchen_dining.world
+roslaunch slam_project world.launch &
+sleep 20
+roslaunch slam_project mapping.launch &
+sleep 3
+rosrun rviz rviz -d /home/chuondao/catkin_ws/src/RoboND-Term2-MapMyWorld/launch/config/lsrobot_slam.rviz &
+
+
