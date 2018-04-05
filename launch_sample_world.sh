cb535cf9fe822293bce9d83aad45b28b683a581a
riochuong <riochuong@gmail.com>
2018-04-05 08:25:52 +0000

    change robot design for ease of control

--- launch_sample_world.sh
+++ launch_sample_world.sh
@@ -1,7 +1,7 @@
-roslaunch slam_project world.launch &
+roslaunch slam_project new_world.launch &
 sleep 10
 roslaunch slam_project mapping.launch &
 sleep 3
-rosrun rviz rviz -d /home/chuondao/catkin_ws/src/RoboND-Term2-MapMyWorld/launch/config/robot_slam.rviz &
+#rosrun rviz rviz -d /home/chuondao/catkin_ws/src/RoboND-Term2-MapMyWorld/launch/config/robot_slam.rviz &
 
 
