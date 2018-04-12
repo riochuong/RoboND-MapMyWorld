c38f18fbe9a3864d92b016c1fcb097e3643d65e3
riochuong@gmail.com <riochuong@gmail.com>
2018-04-12 06:43:28 +0000

    fix launch file

--- launch_sample_world.sh
+++
@@ -1,7 +0,0 @@
-roslaunch slam_project new_world.launch &
-sleep 10
-roslaunch slam_project mapping.launch &
-sleep 3
-#rosrun rviz rviz -d /home/chuondao/catkin_ws/src/RoboND-Term2-MapMyWorld/launch/config/robot_slam.rviz &
-
-
